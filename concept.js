1aaf93ffbb24371292e6a9d9c987b1d3363d1055
Devanshu <devanshukansal11@gmail.com>
2026-03-06 14:40:16 +0530

    Initial Commit

---
+++ concept.js
@@ -0,0 +1,7 @@
+// Nodejs is not a programming language, it is a tool that runs javascript outside the browser and it is a javaScript runtime built on Chrome’s V8 engine and V8 js engine is written in C++ language, in simple terms, we can say that V8 engine converts js code into machine level code so the computer can run it
+// Nodejs is an event driven architecture- it means that it waits for an event and when an event occurs, it runs a function
+// It is also capable of asynchronous I/O or non-blocking I/O
+// Wherever there is javascript, there is a js engine
+// npm is a tool that helps you install and manage packages for your project
+// ES standards means rules and features for the JavaScript language that are officially defined so all browsers and runtimes behave the same
+// Node REPL-Read,Evaluate,Print,Loop and it lets you run the js line by line and see the results immediately
